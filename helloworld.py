aa437e35d54638565f43dc5bae7f1b6ec46e48c7
CriticalD20 <jamesnapier@hanovernorwichschools.org>
2015-09-04 15:30:50 -0400

    Updated from Brython Server: 9/4/2015 3:30:50 PM

--- helloworld.py
+++ helloworld.py
@@ -1,12 +1,14 @@
 """
 helloworld.py
-Author: <J.C. Napier>
+Author: <your name here>
 Credit: <list sources used, if any>
-
+Author: J Napier    
+Credit: tiggerntatie2
 Assignment:
 
 Write and submit a Python program that prints the following:
 
 Hello, world!
 """
+
 print("Hello, world!")
