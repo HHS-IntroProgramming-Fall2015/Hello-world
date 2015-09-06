3c167e0171216b10f61515bc36f5f33519c6b9a6
CriticalD20 <jamesnapier@hanovernorwichschools.org>
2015-09-06 10:40:53 -0400

    Updated from Brython Server: 9/6/2015 10:40:52 AM

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,8 @@
 """
 helloworld.py
-Author: <your name here>
-Credit: <list sources used, if any>
-Author:JNapier    
-Credit:Onmyown
+Author: J Napier    
+Credit: tiggerntatie2
+
 Assignment:
 
 Write and submit a Python program that prints the following:
