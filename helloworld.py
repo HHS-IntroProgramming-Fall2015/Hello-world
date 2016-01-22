6cdcb3f9a2a52a838e527d52b6e9aacdf7eea443
averywallis <averybwallis@gmail.com>
2016-01-22 14:30:14 -0500

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,27 @@
 """
-helloworld.py
+
+hellofriend.py
 Author: Avery Wallis
-Credit: Worked alone
+Credit: None
 
 """
-print('Hello, world!')
+
+a = input('What is your name?')
+b = input('How old are you?')
+b = float(b)
+d = (25-b)
+e = "Hello, "
+f = ". I am "
+g = " years older than you!"
+h = " years younger than you!"
+i = "as old as you are!"
+
+if d > 0:
+    print(e + a + f + str(d) + g)
+    
+if d < 0:
+    d = abs(d)
+    print(e + a + f + str(d) + h)
+    
+if d == 0:
+    print(e + a + f + i)
