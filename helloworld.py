227aeafe9198e02426fd08c13967420dc4bb22ca
CANDYISLIFE <billybender@hanovernorwichschools.org>
2016-01-22 14:40:06 -0500

    Updated from Brython Server: 1/22/2016 2:40:05 PM

--- helloworld.py
+++ helloworld.py
@@ -18,8 +18,8 @@ The final line ("Hello...") is generated dynamically when you run the program, b
 on the name and age that the user enters.
 """
 
-a=input("Please tell me your name.")
-b=input("Please tell me your age.")
+a=input("Please tell me your name:")
+b=input("Please tell me your age:")
 b=int(b)
 c= 25-b
 c = str(c)
