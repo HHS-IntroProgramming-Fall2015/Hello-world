5ceff5c7ecb63f39d2e18f3606605f0400488ea7
tesssny <teresasnyder@hanovernorwichschools.org>
2016-01-22 14:23:14 -0500

    Updated from Brython Server: 1/22/2016 2:23:14 PM

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,7 @@
 """
 helloworld.py
-Author: <Tess Snyder>
-Credit: <Mr. Dennison>
+Author: Tess Snyder
+Credit: none
 Assignment:
 Write and submit a Python program that prints the following:
 Hello, world!
