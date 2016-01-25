ee126b2805614d3e22bd0b9602e3eeae3ef76560
averywallis <averybwallis@gmail.com>
2016-01-25 14:07:05 -0500

    Updated from Brython Server: 1/25/2016 2:07:05 PM

--- helloworld.py
+++ helloworld.py
@@ -1,15 +1,9 @@
 helloworld.py
-"""
-
-helloworld.py
 Author: Avery Wallis
 Credit: None
 
 Assignment:
-
 Write and submit a Python program that prints the following:
-
 Hello, world!
-"""
 
 print('Hello, world!')
