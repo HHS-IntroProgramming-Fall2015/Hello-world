a7ee11c18d4b965d8896fd0eb71e3abecad6b925
liama482 <liamabbate@hanovernorwichschools.org>
2016-09-02 13:52:46 -0400

    Updated from Brython Server: 9/2/2016 1:52:46 PM

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,11 @@
-#helloworld.py
-#Author: <Edit>
-#Credit: <Wilson>
+"""
+helloworld.py
+Author: Liam
+Credit: Wilson
 
-#Assignment:
+Assignment:
 
-#Write and submit a Python program that prints the following:
+Write and submit a Python program that prints the following: """
 
 print("Hello, world!")
 
