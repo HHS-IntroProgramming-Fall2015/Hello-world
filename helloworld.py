2bf3029ded6f8c177a07217bc8f07fbb71013a93
AverageNormalSchoolBoy <brendandufty@hanovernorwichschools.org>
2017-01-26 14:38:46 -0500

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,10 @@
 """
 helloworld.py
-Author: <your name here>
+Author: <Brendan Dufty>
 Credit: <list sources used, if any>
 
 Assignment:
 
-Write and submit a Python program that prints the following:
 
 Hello, world!
 """
