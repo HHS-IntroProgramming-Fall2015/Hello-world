e2fb8523df0d2665b001a5f811f39de09a801f65
Earljb <earlbarrowes@hanovernorwichschools.org>
2017-01-27 14:47:54 -0500

    Updated from Brython Server: 1/27/2017 2:47:54 PM

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,7 @@
 """
 helloworld.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Earl Barrowes
+Credit: None
 
 Assignment:
 
@@ -10,4 +10,4 @@ Write and submit a Python program that prints the following:
 Hello, world!
 """
 
-print("Hello World")
+print("Hello World!")
