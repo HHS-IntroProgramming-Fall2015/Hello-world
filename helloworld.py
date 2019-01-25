5b390ea4c48761cb9eaf0d3059b657a0977fecb1
maBottnn14 Hhs <46973490+MaBottnn14@users.noreply.github.com>
2019-01-25 14:41:23 -0500

    Updated from Brython Server: 1/25/2019 2:41:23 PM

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,7 @@
 """
 helloworld.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: mabottnn14
+Credit:none
 
 Assignment:
 
@@ -9,4 +9,7 @@ Write and submit a Python program that prints the following:
 
 Hello, world!
 """
+
+print("Hello, world!")
+
 
